d33d1ed277db23a460239e096333f4ed3ffd640a
Rashmi Kaveesha <152951952+zrashz@users.noreply.github.com>
2024-07-19 11:24:55 +0530

    Add files via upload

---
+++ keyboard play/script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.key').forEach(key => {
+    key.addEventListener('click', () => playSound(key.getAttribute('data-note')));
+});
+
+function playSound(frequency) {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 0.5);
+}
+
